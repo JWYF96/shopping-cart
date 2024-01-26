929d1a197380cd0ea24766cadabaddeb911a9372
John Wong <john6526@gmail.com>
2024-01-27 02:21:42 +0800

    defining items buttons

--- src/componenets/StoreItem.tsx
+++ src/componenets/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 interface Props {
   id: number;
@@ -9,14 +9,45 @@ interface Props {
 }
 
 const StoreItem = ({ id, name, price, imgUrl, description }: Props) => {
+  const quantity = 1;
+
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
         height="300px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-3">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">${price}</span>
+        </Card.Title>
+        <Card.Subtitle className="mb-4">{description}</Card.Subtitle>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add to Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button className="py-1">-</Button>
+                <div>
+                  <span className="fs-3">{quantity} </span>in cart
+                </div>
+                <Button className="py-1">+</Button>
+              </div>
+              <Button className="btn-danger">Remove</Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
     </Card>
   );
 };
